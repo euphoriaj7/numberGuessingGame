8182950dee8a67f9860fea2e4781213ed36aee81
Jeff Pitcher <pit19004@byui.edu>
2022-06-16 16:32:28 -0600

    fixed intial while loop

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ fn main() {
 //     keep_playing = 'yes'
     let mut keep_playing = String::from("yes");
 // while keep_playing =='yes':
-    while keep_playing == "yes"{
+    while keep_playing.trim().eq("yes"){
 //     magic_number = random.randint(1,100)
         let mut magic_number: i8 =rand::thread_rng().gen_range(-128..127); 
 //     guess_count = 0
         let mut guess_count = 0;
-    
+        println!("{}", magic_number);
 //     while guess !=magic_number:
         loop {
 //     guess = -1
@@ -58,11 +58,12 @@ fn main() {
         //     keep_playing = yes;
         // }
         keep_playing.clear();
-        println!("Would you like to play again (yes/no");
+        println!("Would you like to play again (yes/no)");
         io::stdin().read_line(&mut keep_playing);
         // let keep_playing: i8 = match keep_playing.trim().parse();
-        println!("{}", keep_playing == "no");
-        println!("{}", keep_playing);
+        // keep_playing = keep_playing.trim();
+        println!("{}", keep_playing == "yes\n");
+        println!("[{}]", keep_playing.trim());
         // break;
     }
 // print('Thank you for playing. Goodbye.')
